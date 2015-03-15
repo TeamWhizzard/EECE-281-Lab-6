d5b7120d9b66036ec13c99ea9286f17434617503
Steven Olsen <stevenolsen7@gmail.com>
2015-03-14 23:52:52 -0700

    Basic Working Oscilloscope Sketch
    Nothing fancy, but the interrupt button works.

--- Lab_6/Lab_6.ino
+++ Lab_6/Lab_6.ino
@@ -2,32 +2,36 @@
 // Live: R,Hz,volts,volts,volts,D
 // Fast: F,Hz,volts,volts,volts,D
 
+#define BUTTON 2
+#define READ_DURATION 15000
+
+volatile int readValues = 0;
+
 void setup() {
   Serial.begin(115200);
-  
-  attachInterrupt(0, liveISR, HIGH);  //init hardware interrupt 0 for digital pin 2
-  attachInterrupt(1, fastISR, HIGH);  //init hardware interrupt 1 for digital pin 3
+  pinMode(BUTTON, INPUT);
+  digitalWrite(BUTTON, HIGH);
+  attachInterrupt(1, buttonISR, FALLING);
 }
 
-void loop() {}
-
-void liveISR() {
-  oscilloscopeLive();
-}
-
-void fastISR() {
- // TODO 
-}
-
-void oscilloscopeLive() {
-  Serial.print("R"); // intial message
-  int timer = millis();
-  byte inputV;
+void loop() {
+  int sensorValue;
   float voltage;
-  
-  while ((timer + 1500) < millis()) { // display live voltage for fifteen seconds
-    Serial.print(",");
-    Serial.print(voltage);
+  if (readValues) {
+    unsigned long timer = millis();
+    while ((timer + READ_DURATION) > millis()) {
+      Serial.println(getOscilloscopeValue());
+    }
+    readValues = 0;
   }
-   
+}
+
+void buttonISR() {
+  readValues = 1;
+}
+
+float getOscilloscopeValue() {
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  return voltage;
 }
