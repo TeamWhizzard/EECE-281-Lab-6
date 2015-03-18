cd7bcdc2d457bf3668816dec999b90cf34711836
John Deppe <johndeppe@gmail.com>
2015-03-18 15:53:38 -0700

    FIXED: ADC wasn't running
    Forgot to write ADSC high! Whoops.

--- Lab_6/Lab_6.ino
+++ Lab_6/Lab_6.ino
@@ -1,7 +1,23 @@
-// processing communication protocol:
-// Live: R,Hz,volts,volts,volts,D
-// Fast: F,Hz,volts,volts,volts,D
-
+/* EECE 281
+ * Lab 6 - The WhizzardScope
+ * 
+ *      Team Whizzard is...
+ *                           .
+ *               /^\     .
+ *          /\   "V"
+ *         /__\   I      L  .           John Deppe
+ *        //..\\  I        a               Theresa Mammarella
+ *        \].`[/  I       b                   Steven Olsen
+ *       /l\/j\  (]    .   6
+ *       /. ~~ ,\/I          .
+ *       \\L__j^\/I       o
+ *        \/--v}  I     o   .
+ *        |    |  I   _________
+ *        |    |  I c(`       ')o
+ *        |    l  I   \.     ,/
+ *       _/j  L l\_!  _//^---^\\_
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
 
 // from: http://playground.arduino.cc/Main/AVR
 #ifndef cbi
@@ -10,7 +26,6 @@
 #ifndef sbi
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
-
 
 #define BUTTON 2
 #define LEDPIN 7
@@ -22,28 +37,33 @@ volatile unsigned long timeButtonLastPressed;
 volatile unsigned long timeStartMeasuring;
 
 // debugging junk
-volatile unsigned long numValuesRead;
+volatile unsigned long numValuesRead = 0;
+volatile unsigned long numValuesMeasured = 0;
 volatile unsigned long lastRead;
 
 void setup() {
   Serial.begin(115200);
   pinMode(BUTTON, INPUT_PULLUP);
-  attachInterrupt(1, buttonISR, CHANGE); // Arduino interrupt 1 connects to Digital Pin 2
+  attachInterrupt(0, buttonISR, CHANGE); // Interrupt 0 connects to Pin 2
   pinMode(LEDPIN, OUTPUT);
-  digitalWrite(LEDPIN, LOW);
+  digitalWrite(LEDPIN, HIGH);
+  
+  // References for the following ATMega328 register manipulations
   // based on: http://garretlab.web.fc2.com/en/arduino/inside/arduino/wiring_analog.c/analogRead.html
   // datasheet: http://www.atmel.com/images/Atmel-8271-8-bit-AVR-Microcontroller-ATmega48A-48PA-88A-88PA-168A-168PA-328-328P_datasheet_Complete.pdf
   // around page 234 is useful
   // also nice: http://openenergymonitor.blogspot.ca/2012/08/low-level-adc-control-admux.html
-
-  // set the analog reference (high two bits of ADMUX) and select the
-  // channel (low 4 bits).  this also sets ADLAR (left-adjust result)
-  // to 0 (the default).
-  // page 248 in datasheet describes ADMUX
-  // chooses:
-  // REFS = 01 (internal voltage reference)
-  // ADLAR = 1 (sets ADC value left-aligned, so we can just read ADCH (high bits)
-  // sets mux to read analog pin 0
+  
+  // ADMUX bits from high to low
+  // --------------------------------------------------------------
+  // REFS1 = 0   select Arduino-std voltage reference
+  // REFS0 = 1   select Arduino-std voltage reference
+  // ADLAR = 1   left-aligned ADC reference (lets us pull just high values, ADCH)
+  // resvd = 0   write 0 for compatibility
+  // MUX3  = 0   select ADC0
+  // MUX2  = 0   select ADC0
+  // MUX1  = 0   select ADC0
+  // MUX0  = 0   select ADC0
   ADMUX = 0b01100000;
 
   // decided on 1Mhz adc clock based on http://www.openmusiclabs.com/learning/digital/atmega-adc/
@@ -51,20 +71,34 @@ void setup() {
   // ADCSRA bits from high to low
   // --------------------------------------------------------------
   // ADEN  = 1   ADC enabled
-  // ADSC  = 0   ADC doesn't need to start conversion yet
+  // ADSC  = 1   ADC start measuring
   // ADATE = 1   ADC autotrigger enabled
   // ADIF  = 0   ADC interrupt flag (off?)
   // ADIE  = 1   ADC interrupt enabled
   // ADPS2 = 1   ADC prescaler bit (divide 16 MHz by 16)
   // ADPS1 = 0   ADC prescaler bit (page 250 in datasheet)
   // ADPS0 = 0   ADC prescaler bit
-  ADCSRA = 0b10101100;
+  ADCSRA = 0b11101100;
 
-  //ADCSRB = 0b00000000 | ADCSRB;
+  // ADCSRB bits from high to low
+  // --------------------------------------------------------------
+  // resvd = 0   write 0 for compatibility
+  // ACME  = 0   not using Analog Comparator
+  // resvd = 0   write 0 for compatibility
+  // resvd = 0   write 0 for compatibility
+  // resvd = 0   write 0 for compatibility
+  // ADTS2 = 0   free running mode
+  // ADTS1 = 0   free running mode
+  // ADTS0 = 0   free running mode
+  ADCSRB = 0b00000000;
 
   // turn off digital input buffers for analog pins.
+  // suggested by: http://www.openmusiclabs.com/learning/digital/atmega-adc/
   // Arduino core might already do this, but let's do it again anyway.
   DIDR0 = 0b00111111;
+  
+  // set global interrupt bit on
+  //sei();
 }
 
 void loop() {
@@ -76,19 +110,21 @@ void loop() {
     // http://playground.arduino.cc/Code/TimingRollover
     if (micros() - timeButtonLastPressed > READ_DURATION) {
       readingValues = false;
-      digitalWrite(LEDPIN, LOW);
+      digitalWrite(LEDPIN, HIGH);
       
       // timing/debug output barf
       //float secondsElapsed = float(micros() - timeStartMeasuring) / 1000000.0;
       //Serial.println(" ");
+      //Serial.println(String(numValuesMeasured) + " values measured");
       //Serial.println(String(secondsElapsed) + " seconds elapsed"); // output total time timer was on
       //Serial.println(String(numValuesRead) + " values read");
       //Serial.println(String(float(numValuesRead) / (float(secondsElapsed))) + " samples per second");
       //numValuesRead = 0;
+      //numValuesMeasured = 0;
     } else {
       Serial.write( char(voltage) );
       numValuesRead++; // could remove this, but kept it in for timing purposes
-      // Serial.flush()
+      // Serial.flush();
       // If we want to prevent blocking on the Serial.write() above, we can Serial.flush(),
       // But it knocks a bunch off our throughput, so I'm going to stop.
     }
@@ -98,6 +134,7 @@ void loop() {
 // ADC interrupt service routine
 ISR(ADC_vect) {
   voltage = ADCH;
+  //numValuesMeasured++;
 }
 
 void buttonISR() {
@@ -107,6 +144,6 @@ void buttonISR() {
     timeStartMeasuring = micros();
     readingValues = true;
     timeButtonLastPressed = micros();
-    digitalWrite(7, HIGH);
+    digitalWrite(7, LOW);
   }
 }
